7c40fcbe828b721a86ef81a0d1d5ea2d72c1e950
Harshit Patel <harshit3228@gmail.com>
2022-11-19 18:54:01 +0530

    winston logger implement

--- middleware/errorMiddleware.js
+++ middleware/errorMiddleware.js
@@ -1,8 +1,17 @@
+const logger = require('../config/logger');
+
 const errorHandler = (err, req, res, next) => {
   const statusCode = res.statusCode ? res.statusCode : 500;
 
-  res.status(statusCode);
+  logger.error({
+    message: err.message,
+    user: req.user.email,
+    ip: req.ip,
+    method: req.method,
+    path: req.path
+  });
 
+  res.status(statusCode);
   res.json({
     message: err.message,
     stack: process.env.NODE_ENV === 'production' ? null : err.stack
